73af4f4919e0a88c65c142d8c0a6dd0cdee4e1e4
wiki-fan <uberslowpoke@gmail.com>
2018-11-25 01:17:05 +0300

    Slicing TList

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cassert>
 #include "typelists-m.h"
+#include "hierarchy-m.h"
 
 using namespace m;
 
 using tl = TypeList<int, float, double, char> ;
+using tl8 = TypeList<int, int, int, int, int, int, int, int> ;
 
 
 template<int n>
@@ -22,21 +24,28 @@ struct Fib<0> {
     static const int value = 1;
 };
 
-template<int fib, int n>
+template<typename parent, typename Base>
+struct InheritanceProxy: public parent, public Base {
+
+};
+
+template<int fib, int n, typename TList>
 struct GetCurrentRow {
     const static int curFibNum = Fib<fib>::value;
-    const static int curCount = curFibNum > n? n: fib;
+    const static int curCount = curFibNum > n? n: curFibNum;
     const static int nextCount = curFibNum > n? 0: n-curFibNum;
-    using next = GetCurrentRow<fib+1, nextCount>;
+    using curTypeList = Take<curCount, TList>;
+    using next = GetCurrentRow<fib+1, nextCount, Skip<curCount, TList>>;
+    using parent = GenLinearHierarchy<curTypeList, InheritanceProxy, NullType>; // TODO: not NullType should be here
 };
 
-template<int fib>
-struct GetCurrentRow<fib, 0>{
+template<int fib, typename TList>
+struct GetCurrentRow<fib, 0, TList>{
 };
 
-template<int n>
-struct Branch {
-    using num = GetCurrentRow<1, n>;
+template<int n, typename TList>
+struct Branch { // TODO: looks like it must be ScatterHierarchy
+    using num = GetCurrentRow<0, n, TList>;
 };
 
 struct Root {
@@ -51,10 +60,10 @@ int main()
     static_assert(Fib<0>::value == 1);
     static_assert(Fib<6>::value == 13);
 
-    std::cout << Branch<8>::num::curCount <<'\n';
-    std::cout << Branch<8>::num::next::curCount <<'\n';
-    std::cout << Branch<8>::num::next::next::curCount <<'\n';
-    std::cout << Branch<8>::num::next::next::next::curCount <<'\n';
+    static_assert(Branch<8, tl8>::num::curCount == 1);
+    static_assert(Branch<8, tl8>::num::next::curCount == 1);
+    static_assert(Branch<8, tl8>::num::next::next::curCount == 2);
+    static_assert(Branch<8, tl8>::num::next::next::next::curCount == 3);
 
     assert(typeid(Skip<2, tl>::value) == typeid(TypeList<double, char>));
     assert(typeid(Skip<1, tl>::value) == typeid(TypeList<float, double, char>));
@@ -64,13 +73,6 @@ int main()
     assert(typeid(Take<1, tl>::value) == typeid(TypeList<int>));
     assert(typeid(Take<2, tl>::value) == typeid(TypeList<int, float>));
     assert(typeid(Take<3, tl>::value) == typeid(TypeList<int, float, double>));
-
-    //assert(typeid(Take<2, tl>::value::head) == typeid(int));
-    //TypeList<int, TypeList<float >>;
-    //assert(typeid(Take<2, tl>::value::tail::head::head) == typeid(EmptyList));
-    //assert(typeid(Take<2, tl>::value::tail) == typeid(EmptyList));
-    //assert(typeid(Take<3, tl>::value::head) == typeid(int));
-    //assert(typeid(Take<3, tl>::value::tail::head::head) == typeid(float));
 
     return 0;
 }
